f9c28378f586fe09c4adf60755e9d87ec513d9d0
Bruno Loureiro <116461376+ahavahdev@users.noreply.github.com>
2022-12-06 22:07:06 -0300

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um valor '))
+n2 = int(input('Digite um valor '))
+
+print('A soma de {} e {} é {} '.format(n1, n2, n1 + n2))
+print('O tipo primitivo desse valor é ', type(n1))
